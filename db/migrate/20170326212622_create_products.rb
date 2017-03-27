ef7cac450bf474ddc6e3aff365d1e590359b3337
Valeriy Kuzin <v.kuzin@nixsolutions.com>
2017-03-27 23:18:13 +0300

    Products added model & controller

--- db/migrate/20170326212622_create_products.rb
+++ db/migrate/20170326212622_create_products.rb
@@ -1,7 +1,21 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
-
+      t.string :name
+      t.integer :price
+      t.text :seo_words
+      t.text :seo_description
+      t.text :description
+      t.text :features
+      t.text :mini_description
+      t.text :mini_features
+      t.string :photo
+      t.string :passport
+      t.string :certificate
+      t.integer :brand_id
+      t.integer :purveyor_id
+      t.integer :type_id
+      t.integer :kind_id
       t.timestamps
     end
   end
